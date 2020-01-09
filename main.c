2487432a8bcb6a8c6fc399c5e76074ba7f8f58da
Addison Jones <jones254@student.pcsstn.net>
2020-01-09 08:39:09 -0600

    Added a VS-specific .gitignore file

--- main.c
+++ main.c
@@ -2,15 +2,20 @@
 #include <stdlib.h>
 
 
-int main(void) 
-{
+int main(void) {
+
 unsigned int number;
+
 printf("enter max number\n");
 scanf("%i", &number);
+
 int index;
-for (index =1; index <number; index++)
-{
+for (index =1; index <number; index++) {
+
   printf("%i\n", index);
+
 }
+
 return 0;
+
 }
